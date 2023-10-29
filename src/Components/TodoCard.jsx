8e6f1fba559b3c8ae1722e36da41079b3732654a
ashish <ashishsamuelthomson@gmail.com>
2023-10-30 03:10:52 +0530

    bg color changed

--- src/Components/TodoCard.jsx
+++ src/Components/TodoCard.jsx
@@ -23,7 +23,7 @@ function TodoCard() {
   return (
     <div class="flex flex-col justify-center items-center">
       <div class="flex justify-center items-center w-96">
-        <div class="max-w-sm p-6 border border-gray-200 rounded-lg shadow dark:border-gray-700 w-full h-48 flex flex-col justify-evenly items-center shadow-2xl">
+        <div class="max-w-sm p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-full h-48 flex flex-col justify-evenly items-center shadow-2xl">
           <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">
             <input
               value={newTask || ""}
@@ -31,7 +31,7 @@ function TodoCard() {
               type="text"
               name="todo"
               id="todo"
-              className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400"
               placeholder="Add a task"
               required
             />
@@ -45,12 +45,12 @@ function TodoCard() {
           </button>
         </div>
       </div>
-      <div class="border rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-4/6 flex flex-col justify-evenly items-center shadow-2xl my-8">
+      <div class="border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-4/6 flex flex-col justify-evenly items-center shadow-2xl my-8">
         <h4 class="my-3">List of Tasks</h4>
         <div class="my-6 text-lg w-64 mx-4">
           {todo?.length > 0 ? (
             todo?.map((item, index) => (
-              <div class="p-3 my-5 border rounded-lg shadow flex flex-col justify-between items-center shadow-2xl">
+              <div class="p-3 my-5 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between items-center shadow-2xl">
                 <div>
                   {item}
                   <button onClick={() => deleteTask(index)}>
